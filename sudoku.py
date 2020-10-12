a1868b37c2d63e9bc6a769af9e183feabbd03052
Dennis <52411861+DerDennisOP@users.noreply.github.com>
2020-10-12 08:48:49 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -10,6 +10,36 @@ sod = [[3,4,0,7,1,0,0,5,8],
        [0,0,0,5,0,0,0,1,0],
        [6,7,0,0,4,9,0,0,2]]
 
+arry = [0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2,
+        0,0,0,1,1,1,2,2,2]
+
+arrrx = [0,0,0,0,0,0,0,0,0,
+         1,1,1,1,1,1,1,1,1,
+         2,2,2,2,2,2,2,2,2,
+         0,0,0,0,0,0,0,0,0,
+         1,1,1,1,1,1,1,1,1,
+         2,2,2,2,2,2,2,2,2,
+         0,0,0,0,0,0,0,0,0,
+         1,1,1,1,1,1,1,1,1,
+         2,2,2,2,2,2,2,2,2,]
+
+arrry = [0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,
+         0,1,2,0,1,2,0,1,2,]
+
 
 def solve_easy(arr1, arr2, arr3):
     comp = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -31,22 +61,32 @@ def solve_easy(arr1, arr2, arr3):
     return (comp, pos)
 
 t1 = time.time()
-changes = True
-while changes:
-    changes = False
-    for x in range(3):
-        for y in range(3):
-            zcount = 0
-            for rx in range(3):
-                for ry in range(3):
-                    if sod[y*3+ry][x*3+rx] == 0:
-                        arr1 = [sod[y*3+0][x*3+0],sod[y*3+0][x*3+1],sod[y*3+0][x*3+2],
-                                sod[y*3+1][x*3+0],sod[y*3+1][x*3+1],sod[y*3+1][x*3+2],
-                                sod[y*3+2][x*3+0],sod[y*3+2][x*3+1],sod[y*3+2][x*3+2]]
-                        res = solve_easy(arr1, [row[x*3+rx] for row in sod], sod[y*3+ry])
-                        if len(res[0]) == 1:
-                            sod[y*3+ry][x*3+rx] = res[0][0]
-                            changes = True
+for n in range(100000):
+    changes = True
+    while changes:
+        changes = False
+        for index in range(81):
+            x = math.floor(index/27)
+            y = arry[index]
+            rx = arrrx[index]
+            ry = arrry[index]
+            if sod[y*3+ry][x*3+rx] == 0:
+                res = 284
+                arr1 = [sod[y*3+0][x*3+0],sod[y*3+0][x*3+1],sod[y*3+0][x*3+2],
+                        sod[y*3+1][x*3+0],sod[y*3+1][x*3+1],sod[y*3+1][x*3+2],
+                        sod[y*3+2][x*3+0],sod[y*3+2][x*3+1],sod[y*3+2][x*3+2]]
+                comp = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                for i in range(9):
+                    if arr1[x] != 0 and arr1[x] in comp:
+                        res -= arr1[x]**2
+                    if sod[i][x*3+rx] != 0 and sod[i][x*3+rx] in comp:
+                        res -= sod[i][x*3+rx]**2
+                    if sod[y*3+ry][i] != 0 and sod[y*3+ry][i] in comp:
+                        res -= sod[y*3+ry][i]**2
+                res = math.sqrt(res)
+                if res == round(res):
+                    sod[y*3+ry][x*3+rx] = res
+                    changes = True
 t2 = time.time()
 print(f"Solved in: {t2-t1}s")
 print(sod)
